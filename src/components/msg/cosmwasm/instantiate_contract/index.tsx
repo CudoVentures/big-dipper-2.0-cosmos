91259c7a031c2ce8e6f08999a9d5c94f6e7178a5
SpaghettiOverload <imyours@mail.bg>
2022-12-07 14:52:21 +0200

    CUDOS-1957-extend-instantiate-contract-message

--- src/components/msg/cosmwasm/instantiate_contract/index.tsx
+++ src/components/msg/cosmwasm/instantiate_contract/index.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+} from 'react';
 import numeral from 'numeral';
 import Trans from 'next-translate/Trans';
 import { Typography } from '@material-ui/core';
 import { Name } from '@components';
 import { MsgInstantiateContract } from '@models';
 import { useProfileRecoil } from '@recoil/profiles';
+import { fetchCW20TokenInfo } from '@src/screens/token_details/utils';
 
 const InstantiateContract = (props: {
-   message: MsgInstantiateContract;
- }) => {
+  message: MsgInstantiateContract;
+}) => {
   const { message } = props;
-
+  const [isCW20, setIsCW20] = useState<boolean>(false);
   const sender = useProfileRecoil(message.sender);
   const senderMoniker = sender ? sender?.name : message.sender;
+
+  useEffect(() => {
+    const setCW20 = async (address: string) => {
+      const result = await fetchCW20TokenInfo(address);
+      if (result.address === address) {
+        setIsCW20(true);
+        return;
+      }
+      setIsCW20(false);
+    };
+
+    if (message.contractAddress) {
+      setCW20(message.contractAddress);
+    }
+  }, []);
 
   return (
     <Typography>
@@ -26,8 +45,16 @@ const InstantiateContract = (props: {
             />
           ),
           <b />,
+          (
+            <Name
+              address={message.contractAddress}
+              name={message.contractAddress}
+            />
+          ),
+          <b />,
         ]}
         values={{
+          contractType: isCW20 ? 'CW20 Token ' : '',
           codeId: numeral(message.codeId).format('0,0'),
         }}
       />
--- src/models/msg/cosmwasm/msg_instantiate_contract.ts
+++ src/models/msg/cosmwasm/msg_instantiate_contract.ts
@@ -1,41 +1,56 @@
+import { fetchCW20TokenInfo } from '@src/screens/token_details/utils';
 import * as R from 'ramda';
 import { Categories } from '../types';
 
 class MsgInstantiateContract {
-   public category: Categories;
-   public type: string;
-   public json: any;
-   public sender: string;
-   public admin: string;
-   public codeId: number;
-   public initMsg: JSON;
-   public initCoins: MsgCoin[];
+  public category: Categories;
+  public type: string;
+  public json: any;
+  public sender: string;
+  public admin: string;
+  public codeId: number;
+  public initMsg: JSON;
+  public initCoins: MsgCoin[];
+  public contractAddress: string | undefined;
 
-   constructor(payload: any) {
-     this.category = 'cosmwasm';
-     this.type = payload.type;
-     this.json = payload.json;
-     this.sender = payload.sender;
-     this.admin = payload.admin;
-     this.codeId = payload.codeId;
-     this.initMsg = payload.initMsg;
-     this.initCoins = payload.initCoins;
-   }
+  constructor(payload: any) {
+    this.category = 'cosmwasm';
+    this.type = payload.type;
+    this.json = payload.json;
+    this.sender = payload.sender;
+    this.admin = payload.admin;
+    this.codeId = payload.codeId;
+    this.initMsg = payload.initMsg;
+    this.initCoins = payload.initCoins;
+    this.contractAddress = payload.contractAddress;
+  }
 
-   static fromJson(json: any) {
-     return new MsgInstantiateContract({
-       json,
-       type: json['@type'],
-       sender: json.sender,
-       admin: json.admin,
-       codeId: R.pathOr(0, ['code_id'], json),
-       initMsg: R.pathOr('', ['init_msg'], json),
-       initCoins: R.pathOr([], ['init_coins'], json).map((x) => ({
-         denom: R.pathOr('', ['denom'], x),
-         amount: R.pathOr(0, ['amount'], x),
-       })),
-     });
-   }
+  static getContractAddress(log: any) {
+
+    const contractAddress = log
+      .events.find((event: { type: string; }) => event.type === 'instantiate')
+      .attributes[0].value.replaceAll('"', '') || undefined
+
+    return contractAddress;
+  }
+
+  static fromJson(json: any, log?: any) {
+    const contractAddress = this.getContractAddress(log);
+
+    return new MsgInstantiateContract({
+      json,
+      type: json['@type'],
+      sender: json.sender,
+      admin: json.admin,
+      codeId: R.pathOr(0, ['code_id'], json),
+      initMsg: R.pathOr('', ['init_msg'], json),
+      initCoins: R.pathOr([], ['init_coins'], json).map((x) => ({
+        denom: R.pathOr('', ['denom'], x),
+        amount: R.pathOr(0, ['amount'], x),
+      })),
+      contractAddress
+    });
+  }
 }
 
 export default MsgInstantiateContract;
